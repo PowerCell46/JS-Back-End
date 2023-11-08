5d1f23e47e374b4b67bcdf398e9332ac4b88fdc1
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-11-08 21:41:46 +0200

    Update login.js

--- Regular Exam/viewsControllersJs/login.js
+++ Regular Exam/viewsControllersJs/login.js
@@ -1,54 +1,45 @@
-const Electronics = require("../schemas/electronics");
+const { verifyPassword, createToken } = require("../createTokenHashPassVerifyPass");
+const User = require("../schemas/user");
 
 
-async function getEditView(req, res) {
-    if (req.user === undefined) {
-        return res.render("404", {err: "You are not authorized to access this Edit page because you are not Logged in!"});
+function getLoginView(req, res) {
+    if (req.user !== undefined) {
+        return res.render("404", {err: "You are already logged in and you are not allowed to access the Login Page!"});
     }
 
+    res.render("login");
+} 
 
-    const electronic = await Electronics.findById(req.params.id).lean();
 
-    if (electronic.owner.toString() !== req.user._id.toString()) {
-        return res.render("404", {err: "You are not authorized to alter this Product because you are not the owner of it!"});
+async function loginHandler(req, res) {
+    if (req.user !== undefined) {
+        return res.render("404", {err: "You are already logged in and you are not allowed to access the Login Page!"});
     }
 
-    res.render("edit", {electronic});
-}
-
-
-async function editHandler(req, res) {
     try {
-        if (req.user === undefined) {
-            return res.render("404", "You are not authorized to access this Edit page because you are not Logged in!");
-        }
-    
-        const electronic = await Electronics.findById(req.params.id).lean();
-    
-        if (electronic.owner.toString() !== req.user._id.toString()) {
-            return res.render("404", {err: "You are not authorized to alter this Product because you are not the owner of it!"});
-        }
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email});
         
-        const {name, type, year, exploitation, damages, imageUrl, price, description} = req.body;
-    
-        try {
-            const updatedElectronic = await Electronics.findByIdAndUpdate(req.params.id, {name, type, damages, imageUrl, description, production: Number(year), exploitation: Number(exploitation), price: Number(price), owner: req.user._id}, {new: true, runValidators: true});
+        const passwordValidity = await verifyPassword(password, user.password);
+        console.log(passwordValidity);
+        if (passwordValidity) {
+            const token = createToken(user._id, email, user.user);
 
-            console.log(`Updated electronic: ${updatedElectronic.name}!`);
+            res.cookie("authenticationTokenCookie", token, {httpOnly: true});
 
-        } catch (err) {    
+            console.log(`User: ${user.user} successfully logged in!`);
 
-            return res.render("404", {err: err.message})
+            res.redirect("/");
+        } else {
+            return res.render("login", {err: "Invalid email or password!"});    
         }
-
-
-        res.redirect(`/details/${req.params.id}`);
     
     } catch(err) {
 
-        res.render("404", {err: err.message});
+        return res.render("login", {err: err.message});
     }
 }
 
 
-module.exports = {getEditView, editHandler};
+module.exports = {getLoginView, loginHandler};
