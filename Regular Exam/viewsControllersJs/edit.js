d08223040ab88cbb7d685fa2583b3f3695e5fc8e
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2023-11-08 21:42:01 +0200

    Update edit.js

--- Regular Exam/viewsControllersJs/edit.js
+++ Regular Exam/viewsControllersJs/edit.js
@@ -3,8 +3,9 @@ const Electronics = require("../schemas/electronics");
 
 async function getEditView(req, res) {
     if (req.user === undefined) {
-        return res.render("404", "You are not authorized to access this Edit page because you are not Logged in!");
+        return res.render("404", {err: "You are not authorized to access this Edit page because you are not Logged in!"});
     }
+
 
     const electronic = await Electronics.findById(req.params.id).lean();
 
@@ -27,20 +28,21 @@ async function editHandler(req, res) {
         if (electronic.owner.toString() !== req.user._id.toString()) {
             return res.render("404", {err: "You are not authorized to alter this Product because you are not the owner of it!"});
         }
+        
+        const {name, type, year, exploitation, damages, imageUrl, price, description} = req.body;
     
         try {
-            const {name, type, year, exploitation, damages, imageUrl, price, description} = req.body;
-    
             const updatedElectronic = await Electronics.findByIdAndUpdate(req.params.id, {name, type, damages, imageUrl, description, production: Number(year), exploitation: Number(exploitation), price: Number(price), owner: req.user._id}, {new: true, runValidators: true});
-    
+
             console.log(`Updated electronic: ${updatedElectronic.name}!`);
-    
-            res.redirect(`/details/${req.params.id}`);
-        
-        } catch (err) {
-    
-            return res.render(`/edit/${req.params.id}`, {err: err.message});
+
+        } catch (err) {    
+
+            return res.render("404", {err: err.message})
         }
+
+
+        res.redirect(`/details/${req.params.id}`);
     
     } catch(err) {
 
