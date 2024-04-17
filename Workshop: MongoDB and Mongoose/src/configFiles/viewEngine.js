6819c4df528b3bc9d1e5eee8bc9e2365c72750d1
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2024-04-17 12:57:59 +0300

    Delete Workshop: MongoDB and Mongoose/src directory

--- Workshop: MongoDB and Mongoose/src/configFiles/viewEngine.js
+++
@@ -1,8 +0,0 @@
-const handlebars = require("express-handlebars");
-
- function setUpViewEngine(app) {
-    app.engine("hbs", handlebars.engine({extname: "hbs"}));
-    app.set("view engine", "hbs");
-}
-
-module.exports = {setUpViewEngine} 
