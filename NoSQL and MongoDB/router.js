87701247102b066a9dbef1be1c966235cc43cc3b
Peter Gerdzhikov <116914391+PowerCell46@users.noreply.github.com>
2024-04-22 13:22:33 +0300

    done with the second workshop for the course

--- NoSQL and MongoDB/router.js
+++ NoSQL and MongoDB/router.js
@@ -1,4 +1,6 @@
 const getAboutView = require("./controllers/aboutController");
+const { getAttachAccessoryView, postAttachAccessoryView } = require("./controllers/attachAccessory");
+const { getCreateAccessoryView, postCreateAccessoryView } = require("./controllers/createAccessory");
 const { getCreateView, postCreateView } = require("./controllers/createController");
 const { getDetailsView } = require("./controllers/detailsController");
 const { getErrorView } = require("./controllers/errorController");
@@ -7,29 +9,22 @@ const { getHomeView, postHomeView } = require("./controllers/homeController");
 const router = require("express").Router();
 
 
-/* #region Main */
-
-router.route("/")
-.get((req, res) => {
-    // ... 
-})
-.post((req, res) => {
-    // ...
-})
-.put((req, res) => {
-    // ...
-})
-.delete((req, res) => {
-    // ...
-});
-/* #endregion */
-
 router.route("/")
 .get(getHomeView)
 .post(postHomeView);
 
 
 router.get("/about", getAboutView);
+
+
+router.route("/create/accessory")
+.get(getCreateAccessoryView)
+.post(postCreateAccessoryView);
+
+
+router.route("/attach/accessory/:id")
+.get(getAttachAccessoryView)
+.post(postAttachAccessoryView);
 
 
 router.route("/create")
